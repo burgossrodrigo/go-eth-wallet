c578a632b2c3d976e1d5699911d2adb9fe37544f
Rodrigo Burgos <rodrigoburgos@live.com>
2024-05-18 13:49:19 -0300

    feat: handling mongo connection

--- pkg/mongo/mongo.go
+++ pkg/mongo/mongo.go
@@ -1,8 +1,17 @@
 package mongo
 
+import {
+	config "wallet/pkg/config"
+}
+
 func connect () {
+	cfg := config.LoadEnv()
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.MongoURI))
+	if err != nil {
+		log.Fatalf("Error connecting to MongoDB: %v", err)
+	}
 }
